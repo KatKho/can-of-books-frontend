739fd5ca43f517511570636add8b650e82712ca0
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-09-01 16:07:26 -0700

    added env variable

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const VITE_AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN
 const VITE_AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID
 const VITE_AUTH_REDIRECT_URI = import.meta.env.VITE_AUTH_REDIRECT_URI
+const VITE_AUTH_REDIRECT_URI_NETLIFY = import.meta.env.VITE_AUTH_REDIRECT_URI_NETLIFY
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,7 +13,7 @@ root.render(
     domain={VITE_AUTH_DOMAIN}
     clientId={VITE_AUTH_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: VITE_AUTH_REDIRECT_URI,
+      redirect_uri: VITE_AUTH_REDIRECT_URI_NETLIFY || VITE_AUTH_REDIRECT_URI,
     }}>
     <App />
     </Auth0Provider>
