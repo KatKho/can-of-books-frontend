e9b73dc1a59bf3e4f018f94a91d38d0b8e125b4c
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-09-01 16:18:13 -0700

    removed env file

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,16 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const VITE_AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN
 const VITE_AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID
 const VITE_AUTH_REDIRECT_URI = import.meta.env.VITE_AUTH_REDIRECT_URI
-const VITE_AUTH_REDIRECT_URI_NETLIFY = import.meta.env.VITE_AUTH_REDIRECT_URI_NETLIFY
+// const VITE_AUTH_REDIRECT_URI_NETLIFY = import.meta.env.VITE_AUTH_REDIRECT_URI_NETLIFY
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('REDIRECT URI:' + VITE_AUTH_REDIRECT_URI);
 root.render(
   <React.StrictMode>
     <Auth0Provider
     domain={VITE_AUTH_DOMAIN}
     clientId={VITE_AUTH_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: VITE_AUTH_REDIRECT_URI_NETLIFY || VITE_AUTH_REDIRECT_URI,
+      redirect_uri: VITE_AUTH_REDIRECT_URI,
     }}>
     <App />
     </Auth0Provider>
